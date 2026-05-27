a2395f8c91f063af1e5b4846702337c34a806e98
marconeri70 <marconeri70@gmail.com>
2026-05-27 19:47:43 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+self.addEventListener("install", event => {
+
+  event.waitUntil(
+
+    caches.open("roomsense-cache").then(cache => {
+
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.webmanifest"
+      ]);
+
+    })
+
+  );
+});
+
+self.addEventListener("fetch", event => {
+
+  event.respondWith(
+
+    caches.match(event.request).then(response => {
+
+      return response || fetch(event.request);
+
+    })
+
+  );
+});
